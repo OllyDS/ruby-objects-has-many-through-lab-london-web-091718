a13db661b0a712da1f32f7fc8e179f2da4f0d097
OllyDS <desousa88@gmail.com>
2018-09-21 14:32:49 +0100

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select {|apt| apt.doctor == self}
+  end
+
+  def patients
+    appointments.map {|patient| patient.patient}
+  end
+
+end
